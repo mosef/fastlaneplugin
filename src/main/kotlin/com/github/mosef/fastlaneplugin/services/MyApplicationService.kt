62df67fd7c8f67a1f473d46263112756486442fe
GitHub Action <action@github.com>
2021-02-16 17:54:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/mosef/fastlaneplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mosef/fastlaneplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mosef.fastlaneplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mosef.fastlaneplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/mosef/fastlaneplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mosef/fastlaneplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mosef.fastlaneplugin.services
 
+import com.github.mosef.fastlaneplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
